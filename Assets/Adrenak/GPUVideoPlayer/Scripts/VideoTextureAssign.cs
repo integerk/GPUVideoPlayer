3bcdec1f7b013a0e941e30b4db50581b2a57d993
morioka <morioka@k-ips.com>
2019-06-18 12:58:38 +0900

    GPUVideoTextureTest

---
+++ Assets/Adrenak/GPUVideoPlayer/Scripts/VideoTextureAssign.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Adrenak.GPUVideoPlayer;
+
+
+public class VideoTextureAssign : MonoBehaviour
+{
+    public GPUVideoPlayer _player=null;
+    public Material _material = null;
+    public Texture2D _texture = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<GPUVideoPlayer>();
+        _material = GetComponent<Renderer>().material;
+        Texture2D tmpTex = _player.MediaTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_texture == null)
+        {
+            Texture2D tmpTex = _player.MediaTexture;
+            if (tmpTex != null)
+            {
+                _texture = tmpTex;
+                _material.SetTexture("_MainTex",_texture);
+            }
+        }
+    }
+}
